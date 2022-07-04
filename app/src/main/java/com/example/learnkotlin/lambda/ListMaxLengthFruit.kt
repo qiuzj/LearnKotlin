54dfa0f01e6292f6d718279facde17526a6a0fd0
qiuzhanjia <qiuzhanjia@lizhi.fm>
2022-07-04 20:31:16 +0800

    ListMaxLengthFruit

---
+++ app/src/main/java/com/example/learnkotlin/lambda/ListMaxLengthFruit.kt
@@ -0,0 +1,26 @@
+package com.example.learnkotlin.lambda
+
+fun main() {
+    val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape", "Watermelon")
+    var maxLengthFruit = ""
+    for (fruit in list) {
+        if (fruit.length > maxLengthFruit.length) {
+            maxLengthFruit = fruit
+        }
+    }
+    println("max length fruit is " + maxLengthFruit)
+
+    val lambda = { fruit: String -> fruit.length }
+    val maxLengthFruit3 = list.maxByOrNull(lambda)
+    println("max length fruit is " + maxLengthFruit3)
+
+    // 很多等价写法
+//    val maxLengthFruit = list.maxBy() { fruit: String -> fruit.length }
+//    val maxLengthFruit = list.maxBy { fruit: String -> fruit.length }
+//    val maxLengthFruit = list.maxBy { fruit -> fruit.length }
+//    val maxLengthFruit = list.maxBy { it.length }
+
+
+    val maxLengthFruit2 = list.maxByOrNull{ it.length }
+    println("max length fruit is " + maxLengthFruit2)
+}
